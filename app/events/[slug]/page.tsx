825adfc37f63345bd6a7fd6fba3a926f09b5216e
ShariqYT <shariqmohd737@gmail.com>
2025-10-26 21:51:43 +0530

    Fixed Typescript error

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import BookEvent from "@/components/BookEvent";
 import {getSimilarEventsBySlug, ILeanEvent} from "@/lib/actions/event.actions";
 import EventCard from "@/components/EventCard";
+import {cacheLife} from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -36,6 +37,9 @@ const EventTags = ({tags}: {tags: string[]}) => (
 )
 
 const EventDetailsPage = async ({params}: {params: Promise<{slug: string}>}) => {
+    'use cache'
+    cacheLife('hours')
+
     const { slug } = await params;
 
     let event: FetchedEvent;
@@ -117,7 +121,7 @@ const EventDetailsPage = async ({params}: {params: Promise<{slug: string}>}) => 
                             <p className={"text-sm"}>Be the first to book your spot!</p>
                         )}
 
-                        <BookEvent />
+                        <BookEvent eventId={(event._id).toString()} slug={event.slug} />
                     </div>
                 </aside>
             </div>
